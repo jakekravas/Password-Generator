dcf38e9a2a91f380da9fa4dde6718921f68774db
Jake Kravas <jakekravas7@gmail.com>
2019-11-12 04:48:29 -0700

    setup functions to randomly grab a character for each piece of criteria

--- Assets/app.js
+++ Assets/app.js
@@ -4,3 +4,24 @@ let lowerChecked = document.querySelector("#lowerCheck");
 let upperChecked = document.querySelector("#upperCheck");
 let numberChecked = document.querySelector("#numberCheck");
 let specialChecked = document.querySelector("#specialCheck");
+
+// SETUP FUNCTIONS FOR EACH PIECE OF CRITERIA
+function getRandomLower() {
+    let randLower = Math.floor(Math.random() * 26) + 97;
+    return String.fromCharCode(randLower);
+}
+
+function getRandomUpper(){
+    let randUpper = Math.floor(Math.random() * 26) + 65;
+    return String.fromCharCode(randUpper);
+}
+
+function getRandomNumber(){
+    let randNumber = Math.floor(Math.random() * 10);
+    return randNumber;
+}
+
+function getRandomSpecial(){
+    let randSpecial = Math.floor(Math.random() * 14) + 33;
+    return String.fromCharCode(randSpecial);
+}
