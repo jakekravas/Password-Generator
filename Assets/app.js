4e1edc502506310cb4b6c570ab41801b5dbc42b8
Jake Kravas <jakekravas7@gmail.com>
2019-11-12 04:41:01 -0700

    declared variables to build foundation for getting criteria from user

---
+++ Assets/app.js
@@ -0,0 +1,6 @@
+// GET PASSWORD CRITERIA FROM USER
+let passwordLength = document.querySelector("#pwLength");
+let lowerChecked = document.querySelector("#lowerCheck");
+let upperChecked = document.querySelector("#upperCheck");
+let numberChecked = document.querySelector("#numberCheck");
+let specialChecked = document.querySelector("#specialCheck");
